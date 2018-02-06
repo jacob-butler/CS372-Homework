9e319fa4cb02f5410d3971098f089837ac1d8640
Jacob <jbutler12@alaska.edu>
2018-02-06 12:33:32 -0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// main.cpp
+// Jacob Butler
+// 6 February, 2018
+//
+// Last Edit:
+//	   Date: 6 February, 2018
+//	    Who: Jacob Butler	
+//
+// demo for merge conflicts in CS372
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "This text if for the initial git commit" << std::endl;
+}
